173ecb51faab4b06058dbdaaf9a00d65e9496603
Serhii Vyshnov <svyshnov@gmail.com>
2022-09-28 14:50:44 +0300

    feat: install @nestjs/config feat: inject aws cognito config props to the cognito service
    ZZMED-22

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,15 @@ import {DataSource} from 'typeorm';
 import {UsersModule} from './users/users.module';
 import {dbConnectionOptions} from './config/db.config';
 import {CognitoService} from './infrastructure/aws/cognito.service';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(dbConnectionOptions),
         UsersModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
     ],
     exports: [
         TypeOrmModule,
--- src/infrastructure/aws/cognito.service.ts
+++ src/infrastructure/aws/cognito.service.ts
@@ -4,25 +4,42 @@ import {
     AdminInitiateAuthCommand,
     AuthFlowType,
 } from '@aws-sdk/client-cognito-identity-provider';
+import {ConfigService} from '@nestjs/config';
+
+interface CognitoProviderConfig {
+    region: string;
+    accessKeyId: string;
+    secretAccessKey: string;
+    userPoolId: string;
+    clientId: string;
+}
 
 @Injectable()
 export class CognitoService {
-    private _cognitoProviderConfig = {
-        region: 'us-east-1',
-    };
+    private readonly config: CognitoProviderConfig;
+
+    constructor(private configService: ConfigService) {
+        this.config = {
+            region: configService.get<string>('AWS_COGNITO_REGION'),
+            accessKeyId: configService.get<string>('AWS_COGNITO_ACCESS_KEY_ID'),
+            secretAccessKey: configService.get<string>('AWS_COGNITO_SECRET_ACCESS_KEY'),
+            userPoolId: configService.get<string>('AWS_COGNITO_USER_POOL_ID'),
+            clientId: configService.get<string>('AWS_COGNITO_CLIENT_ID'),
+        };
+    }
 
     public async signIn(): Promise<string> {
         const client = new CognitoIdentityProviderClient({
-            region: 'us-east-1',
+            region: this.config.region,
             credentials: {
-                accessKeyId: '<access_id>',
-                secretAccessKey: '<secret>',
+                accessKeyId: this.config.accessKeyId,
+                secretAccessKey: this.config.secretAccessKey,
             },
         });
 
         const command = new AdminInitiateAuthCommand({
-            UserPoolId: '<pool>',
-            ClientId: '<client_id>',
+            UserPoolId: this.config.userPoolId,
+            ClientId: this.config.clientId,
             AuthFlow: AuthFlowType.ADMIN_USER_PASSWORD_AUTH,
             AuthParameters: {
                 [USER_ATTRIBUTES.USER_NAME]: 'thetytn@gmail.com',
