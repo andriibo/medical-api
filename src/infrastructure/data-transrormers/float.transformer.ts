97466138eb18a3717c6399ee03220df1bee67fea
Serhii Zakharov <s.zakharov@sportysystems.com>
2022-11-02 11:29:19 +0200

    style: access level modifiers added

--- src/infrastructure/data-transrormers/float.transformer.ts
+++ src/infrastructure/data-transrormers/float.transformer.ts
@@ -1,11 +1,11 @@
 import {ValueTransformer} from 'typeorm';
 
 export class FloatTransformer implements ValueTransformer {
-    to(value?: number): string {
+    public to(value?: number): string {
         return value?.toString();
     }
 
-    from(value?: string): number | null {
+    public from(value?: string): number | null {
         return value ? Number.parseFloat(value) : null;
     }
 }
