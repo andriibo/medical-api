ca48b713a9a44bee794687aef5ce0fc70674c91d
andrey <bondarenko.andrey@sportysystems.com>
2022-11-17 17:22:32 +0200

    ZZMED-157: emit patient

---
+++ src/application/modules/patient-data-access/services/access-to-patient-binding.service.ts
@@ -0,0 +1,21 @@
+import {User} from 'domain/entities';
+import {IPatientDataAccessRepository} from 'app/modules/patient-data-access/repositories';
+
+export class AccessToPatientBindingService {
+    public constructor(private readonly patientDataAccessRepository: IPatientDataAccessRepository) {}
+
+    public async bindAccessToPatient(patient: User): Promise<void> {
+        const items = await this.patientDataAccessRepository.getByGrantedEmail(patient.email);
+
+        if (!items.length) {
+            return;
+        }
+
+        items.map((item) => {
+            item.patientUserId = patient.id;
+            item.patientEmail = null;
+        });
+
+        await this.patientDataAccessRepository.update(items);
+    }
+}
