c1863bc8a3f5224009343656c3bba6b980bb30fd
andrii <bondarenkoa1990@gmail.com>
2022-11-30 15:05:50 +0200

    ZZMED-220: View account info by Caregiver

--- src/infrastructure/factories/profile/caregiver-use-cases.factory.ts
+++ src/infrastructure/factories/profile/caregiver-use-cases.factory.ts
@@ -1,6 +1,5 @@
 import {Inject, Injectable} from '@nestjs/common';
 import {IAuthedUserService} from 'app/modules/auth/services/authed-user.service';
-import {PatientDataAccessSpecification} from 'app/modules/patient-data-access/specifications/patient-data-access.specification';
 import {IFileUrlService} from 'app/modules/profile/services/file-url.service';
 import {CaregiverProfileUseCase} from 'app/modules/profile/use-cases/caregiver';
 
@@ -8,7 +7,6 @@ import {CaregiverProfileUseCase} from 'app/modules/profile/use-cases/caregiver';
 export class CaregiverUseCasesFactory {
     public constructor(
         @Inject(IAuthedUserService) private readonly authedUserService: IAuthedUserService,
-        private readonly patientDataAccessSpecification: PatientDataAccessSpecification,
         @Inject(IFileUrlService) private readonly fileUrlService: IFileUrlService,
     ) {}
 
