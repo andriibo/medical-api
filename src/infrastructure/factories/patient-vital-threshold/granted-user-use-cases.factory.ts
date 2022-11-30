1c5437142f5a5e5419c9deb59a9281e3860c980e
andrii <bondarenkoa1990@gmail.com>
2022-11-30 13:04:33 +0200

    ZZMED-219: View patient thresholds for caregiver

--- src/infrastructure/factories/patient-vital-threshold/granted-user-use-cases.factory.ts
+++ src/infrastructure/factories/patient-vital-threshold/granted-user-use-cases.factory.ts
@@ -2,7 +2,6 @@ import {Inject, Injectable} from '@nestjs/common';
 import {IAuthedUserService} from 'app/modules/auth/services/authed-user.service';
 import {IPatientVitalThresholdRepository} from 'app/modules/patient-vital-threshold/repositories';
 import {PatientVitalThresholdSpecification} from 'app/modules/patient-vital-threshold/specifications/patient-vital-threshold.specification';
-import {IPatientVitalThresholdEntityMapper} from 'app/modules/patient-vital-threshold/mappers/patient-vital-threshold-entity.mapper';
 import {IUserRepository} from 'app/modules/auth/repositories';
 import {PatientVitalThresholdsDtoMapper} from 'app/modules/patient-vital-threshold/mappers/patient-vital-thresholds-dto.mapper';
 import {ThresholdListUseCase} from 'app/modules/patient-vital-threshold/use-cases/granted-user';
@@ -14,8 +13,6 @@ export class GrantedUserUseCasesFactory {
         @Inject(IAuthedUserService) private readonly authedUserService: IAuthedUserService,
         @Inject(IPatientVitalThresholdRepository)
         private readonly patientVitalThresholdRepository: IPatientVitalThresholdRepository,
-        @Inject(IPatientVitalThresholdEntityMapper)
-        private readonly PatientVitalThresholdEntityMapper: IPatientVitalThresholdEntityMapper,
         @Inject(PatientVitalThresholdSpecification)
         private readonly patientVitalThresholdSpecification: PatientVitalThresholdSpecification,
         @Inject(PatientVitalThresholdsDtoMapper)
