47c7d63eea9619d62389a4591f825a93088f5874
andrii <bondarenkoa1990@gmail.com>
2022-12-01 16:12:19 +0200

    ZZMED-237: Get Data Accesses for Caregiver Role

--- src/presentation/controllers/patient-data-access/doctor.controller.ts
+++ src/presentation/controllers/patient-data-access/doctor.controller.ts
@@ -1,25 +1,13 @@
-import {Controller, HttpStatus, Get, Delete, BadRequestException, Param, ParseUUIDPipe, HttpCode} from '@nestjs/common';
+import {Controller, HttpStatus, Delete, BadRequestException, Param, ParseUUIDPipe, HttpCode} from '@nestjs/common';
 import {ApiBearerAuth, ApiResponse, ApiTags} from '@nestjs/swagger';
 import {Roles} from 'presentation/guards';
 import {DoctorUseCasesFactory} from 'infrastructure/factories/patient-data-access/doctor-use-cases.factory';
-import {DataAccessView} from 'presentation/views/response/data-access';
-import {DataAccessDto} from 'domain/dtos/response/data-access/data-access.dto';
 
 @Controller('doctor')
 @ApiBearerAuth()
 @ApiTags('Patient Data Access')
 export class DoctorController {
     public constructor(private readonly doctorUseCasesFactory: DoctorUseCasesFactory) {}
-
-    @Roles('Doctor')
-    @Get('data-accesses')
-    @HttpCode(HttpStatus.OK)
-    @ApiResponse({status: HttpStatus.OK, type: [DataAccessView]})
-    public async getDataAccesses(): Promise<DataAccessDto[]> {
-        const useCase = this.doctorUseCasesFactory.createDataAccessListUseCase();
-
-        return await useCase.getList();
-    }
 
     @Roles('Doctor')
     @Delete('data-access/:accessId')
