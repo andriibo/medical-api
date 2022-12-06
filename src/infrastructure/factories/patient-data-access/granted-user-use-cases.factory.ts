180a928f5628868904ad78ace724387240e12098
andrii <bondarenkoa1990@gmail.com>
2022-12-06 16:25:44 +0200

    hotfix

--- src/infrastructure/factories/patient-data-access/granted-user-use-cases.factory.ts
+++ src/infrastructure/factories/patient-data-access/granted-user-use-cases.factory.ts
@@ -20,7 +20,7 @@ export class GrantedUserUseCasesFactory {
     public constructor(
         @Inject(IUserRepository) private readonly userRepository: IUserRepository,
         @Inject(IAuthedUserService) private readonly authedUserService: IAuthedUserService,
-        @Inject(AccessToRegisteredPatientService)
+        @Inject(PatientDataAccessSpecification)
         private readonly accessToRegisteredPatientService: AccessToRegisteredPatientService,
         @Inject(AccessToUnregisteredPatientService)
         private readonly accessToUnregisteredPatientService: AccessToUnregisteredPatientService,
