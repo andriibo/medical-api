28647838d246ff0f6a949c192b590608275f8f01
andrii <bondarenkoa1990@gmail.com>
2023-01-10 19:17:22 +0200

    ZZMED-331: CRON_AVAILABLE

--- src/application/modules/cron/services/cron.service.ts
+++ src/application/modules/cron/services/cron.service.ts
@@ -1,15 +1,21 @@
 import {Injectable} from '@nestjs/common';
 import {Cron, CronExpression} from '@nestjs/schedule';
 import {CronUseCasesFactory} from 'infrastructure/modules/cron/factories/cron-use-cases.factory';
+import {ConfigService} from '@nestjs/config';
 
 @Injectable()
 export class CronService {
-    public constructor(private readonly cronUseCasesFactory: CronUseCasesFactory) {}
+    public constructor(
+        private readonly cronUseCasesFactory: CronUseCasesFactory,
+        private readonly configService: ConfigService,
+    ) {}
 
     @Cron(CronExpression.EVERY_MINUTE)
     public async removeUsersMarkedDeletedAt(): Promise<void> {
+        if (!this.configService.get<boolean>('CRON_AVAILABLE')) {
+            return;
+        }
         const useCase = this.cronUseCasesFactory.removeUsers();
-
         try {
             return await useCase.remove();
         } catch (error) {
