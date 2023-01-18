296bf74a37236e1fd0a66c3ae942438d4664d7d6
andrii <bondarenkoa1990@gmail.com>
2023-01-18 16:14:39 +0200

    caregiver userId

--- src/domain/dtos/response/profile/caregiver.dto.ts
+++ src/domain/dtos/response/profile/caregiver.dto.ts
@@ -4,6 +4,7 @@ import {UserDto} from 'domain/dtos/response/user/user.dto';
 export class CaregiverDto extends UserDto {
     public static fromUser<T extends typeof CaregiverDto>(this: T, user: User): InstanceType<T> {
         const dto = new this() as InstanceType<T>;
+        dto.userId = user.id;
         dto.email = user.email;
         dto.firstName = user.firstName;
         dto.lastName = user.lastName;
