91b5a62f07e3efef706e3861e42a47e4037d9f66
random <andrey.bondarenko@techs.com.ua>
2023-01-21 16:44:54 +0200

    ZZMED-382: medication.controller.e2e-spec.ts

---
+++ test/controllers/medication.controller.e2e-spec.ts
@@ -0,0 +1,77 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import * as request from 'supertest';
+import {INestApplication, ValidationPipe} from '@nestjs/common';
+import {MedicationModule} from 'infrastructure/modules';
+import {IUserRepository} from 'app/modules/auth/repositories';
+import {getRepositoryToken} from '@nestjs/typeorm';
+import {DoctorMetadataModel, PatientMetadataModel, UserModel} from 'infrastructure/modules/auth/models';
+import {User} from 'domain/entities';
+import {IDoctorMetadataRepository, IPatientMetadataRepository} from 'app/modules/profile/repositories';
+import {TestModule} from 'tests/test.module';
+import {MedicationModel} from 'infrastructure/modules/medication/models/medication.model';
+import {IMedicationRepository} from 'app/modules/medication/repositories';
+
+const registeredUser: User = {
+    id: '8bfbd95c-c8a5-404b-b3eb-6ac648052ac4',
+    email: 'doctor@gmail.com',
+    firstName: 'Marc',
+    lastName: 'Goldman',
+    phone: '2930412345',
+    avatar: null,
+    role: 'Doctor',
+    createdAt: '2022-10-10 07:31:17.016236',
+    deletedAt: null,
+};
+const medications = [
+    {
+        genericName: 'Abacavir',
+        brandNames: ['ZIAGEN'],
+    },
+];
+describe('MedicationController', () => {
+    let app: INestApplication;
+    beforeAll(async () => {
+        const mockedUserRepository = {
+            getOneById: jest.fn(() => Promise.resolve(registeredUser)),
+        };
+        const mockedMedicationRepository = {
+            get: jest.fn(() => Promise.resolve(medications)),
+        };
+        const moduleRef: TestingModule = await Test.createTestingModule({
+            imports: [TestModule, MedicationModule],
+        })
+            .overrideProvider(getRepositoryToken(UserModel))
+            .useValue(null)
+            .overrideProvider(getRepositoryToken(DoctorMetadataModel))
+            .useValue(null)
+            .overrideProvider(getRepositoryToken(PatientMetadataModel))
+            .useValue(null)
+            .overrideProvider(getRepositoryToken(MedicationModel))
+            .useValue(null)
+            .overrideProvider(IUserRepository)
+            .useValue(mockedUserRepository)
+            .overrideProvider(IPatientMetadataRepository)
+            .useValue(null)
+            .overrideProvider(IDoctorMetadataRepository)
+            .useValue(null)
+            .overrideProvider(IMedicationRepository)
+            .useValue(mockedMedicationRepository)
+            .compile();
+
+        app = moduleRef.createNestApplication();
+        app.useGlobalPipes(new ValidationPipe());
+        await app.init();
+    });
+
+    it(`/medications (GET)`, async () => {
+        return request(app.getHttpServer())
+            .get('/medications')
+            .set('Authorization', 'Bearer doctor')
+            .expect(200)
+            .expect(medications);
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+});
