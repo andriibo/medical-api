855947f3b458cb378590514d464ef66ad6ca2c35
random <andrey.bondarenko@techs.com.ua>
2023-01-25 16:10:07 +0200

    ZZMED-401: join condition

--- src/infrastructure/modules/patient-data-access/models/patient-data-access.repository.ts
+++ src/infrastructure/modules/patient-data-access/models/patient-data-access.repository.ts
@@ -47,7 +47,7 @@ export class PatientDataAccessRepository implements IPatientDataAccessRepository
     public async getByPatientUserId(patientUserId: string): Promise<PatientDataAccess[]> {
         return await this.dataSource
             .createQueryBuilder(PatientDataAccessModel, 'pda')
-            .leftJoinAndSelect('pda.grantedUser', 'user')
+            .leftJoinAndSelect('pda.grantedUser', 'user', 'user.deleted_at is null and user.email is not null')
             .where('pda.patient_user_id = :patientUserId', {patientUserId})
             .andWhere('user.deleted_at is null')
             .andWhere('user.email is not null')
@@ -60,10 +60,8 @@ export class PatientDataAccessRepository implements IPatientDataAccessRepository
     public async getByGrantedUserId(grantedUserId: string): Promise<PatientDataAccess[]> {
         return await this.dataSource
             .createQueryBuilder(PatientDataAccessModel, 'pda')
-            .leftJoinAndSelect('pda.patientUser', 'user')
+            .leftJoinAndSelect('pda.patientUser', 'user', 'user.deleted_at is null and user.email is not null')
             .where('pda.granted_user_id = :grantedUserId', {grantedUserId})
-            .andWhere('user.deleted_at is null')
-            .andWhere('user.email is not null')
             .orderBy({
                 'pda.createdAt': 'DESC',
             })
