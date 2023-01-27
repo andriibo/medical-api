3d24947d8633d7742c140c8a3ea526f7df8d7735
random <andrey.bondarenko@techs.com.ua>
2023-01-27 14:47:46 +0200

    ZZMED-398: GET patient-category

--- src/domain/entities/patient-category.entity.ts
+++ src/domain/entities/patient-category.entity.ts
@@ -1,17 +1,9 @@
-import {User} from 'domain/entities/user.entity';
-
 export interface PatientCategory {
     id: string;
 
-    patientUserId?: string;
-
-    grantedUserId?: string;
+    patientUserId: string;
 
     category: string;
 
     patientCategoryUpdatedAt: number;
-
-    grantedUser?: User | null;
-
-    patientUser?: User | null;
 }
