ada330c097b3bd970b846c42fc3994a7df9cf889
random <andrey.bondarenko@techs.com.ua>
2023-02-02 16:00:00 +0200

    ZZMED-427: patientDataAccessSpecification --

--- src/infrastructure/modules/patient-data-access/services/data-access-approved.service.ts
+++ src/infrastructure/modules/patient-data-access/services/data-access-approved.service.ts
@@ -4,7 +4,6 @@ import {PatientDataAccess} from 'domain/entities/patient-data-access.entity';
 import {PatientCategoryEnum} from 'domain/entities/patient-category.entity';
 import {currentUnixTimestamp} from 'app/support/date.helper';
 import {IPatientStatusRepository} from 'app/modules/patient-status/repositories';
-import {EntityNotFoundError} from 'app/errors';
 import {IDataAccessApprovedService} from 'app/modules/patient-data-access/services/data-access-approved.service';
 
 export class DataAccessApprovedService implements IDataAccessApprovedService {
@@ -17,9 +16,6 @@ export class DataAccessApprovedService implements IDataAccessApprovedService {
 
     public async handle(dataAccess: PatientDataAccess): Promise<void> {
         const patientStatus = await this.patientStatusRepository.getByPatientUserId(dataAccess.patientUserId);
-        if (patientStatus === null) {
-            throw new EntityNotFoundError('PatientStatus Not Found.');
-        }
         await this.patientCategoryRepository.updateCategoryAndUpdatedAtById(
             patientStatus.status as PatientCategoryEnum,
             currentUnixTimestamp(),
--- src/infrastructure/modules/patient-status/models/patient-status.repository.ts
+++ src/infrastructure/modules/patient-status/models/patient-status.repository.ts
@@ -2,15 +2,24 @@ import {Injectable} from '@nestjs/common';
 import {InjectDataSource} from '@nestjs/typeorm';
 import {DataSource} from 'typeorm';
 import {PatientStatusModel} from './patient-status.model';
-import {PatientStatus} from 'domain/entities/patient-status.entity';
+import {PatientStatus, PatientStatusEnum} from 'domain/entities/patient-status.entity';
 import {IPatientStatusRepository} from 'app/modules/patient-status/repositories';
+import {currentUnixTimestamp} from 'app/support/date.helper';
 
 @Injectable()
 export class PatientStatusRepository implements IPatientStatusRepository {
     public constructor(@InjectDataSource() private dataSource: DataSource) {}
 
     public async getByPatientUserId(patientUserId: string): Promise<PatientStatus> {
-        return await this.dataSource.manager.findOneBy(PatientStatusModel, {patientUserId});
+        let entity = await this.dataSource.manager.findOneBy(PatientStatusModel, {patientUserId});
+        if (entity === null) {
+            entity = new PatientStatusModel();
+            entity.patientUserId = patientUserId;
+            entity.status = PatientStatusEnum.Normal;
+            entity.setAt = currentUnixTimestamp();
+        }
+
+        return entity;
     }
 
     public async persist(entity: PatientStatusModel): Promise<PatientStatus> {
