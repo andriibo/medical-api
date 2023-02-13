5f89d3a500e66ffc20bbdf1010875ebbf88cef79
random <andrey.bondarenko@techs.com.ua>
2023-02-13 17:06:36 +0200

    ZZMED-388: emergency-contact

--- src/infrastructure/modules/emergency-contact/factories/granted-user-use-cases.factory.ts
+++ src/infrastructure/modules/emergency-contact/factories/granted-user-use-cases.factory.ts
@@ -1,27 +1,21 @@
 import {Inject, Injectable} from '@nestjs/common';
 import {PatientContactListUseCase} from 'app/modules/emergency-contact/use-cases/granted-user';
 import {IAuthedUserService} from 'app/modules/auth/services/authed-user.service';
-import {IUserRepository} from 'app/modules/auth/repositories';
-import {IPatientDataAccessRepository} from 'app/modules/patient-data-access/repositories';
 import {IEmergencyContactRepository} from 'app/modules/emergency-contact/repositories';
 import {PatientDataAccessSpecification} from 'app/modules/patient-data-access/specifications/patient-data-access.specification';
 
 @Injectable()
 export class GrantedUserUseCasesFactory {
     public constructor(
-        @Inject(IUserRepository) private readonly userRepository: IUserRepository,
         @Inject(IAuthedUserService) private readonly authedUserService: IAuthedUserService,
         @Inject(IEmergencyContactRepository)
         private readonly emergencyContactRepository: IEmergencyContactRepository,
-        @Inject(IPatientDataAccessRepository)
-        private readonly patientDataAccessRepository: IPatientDataAccessRepository,
         @Inject(PatientDataAccessSpecification)
         private readonly patientDataAccessSpecification: PatientDataAccessSpecification,
     ) {}
 
     public createPatientContactUseCase(): PatientContactListUseCase {
         return new PatientContactListUseCase(
-            this.userRepository,
             this.authedUserService,
             this.emergencyContactRepository,
             this.patientDataAccessSpecification,
---
+++ test/controllers/emergency-contact/patient.controller.e2e-spec.ts
@@ -0,0 +1,157 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import * as request from 'supertest';
+import {INestApplication, ValidationPipe} from '@nestjs/common';
+import {EmergencyContact, User} from 'domain/entities';
+import {TestModule} from 'tests/test.module';
+import {getRepositoryToken} from '@nestjs/typeorm';
+import {DoctorMetadataModel, PatientMetadataModel, UserModel} from 'infrastructure/modules/auth/models';
+import {IPatientVitalThresholdsRepository} from 'app/modules/patient-vital-thresholds/repositories';
+import {EmergencyContactModel} from 'infrastructure/modules/emergency-contact/models';
+import {PatientDataAccessModel} from 'infrastructure/modules/patient-data-access/models';
+import {PatientStatusModel} from 'infrastructure/modules/patient-status/models';
+import {PatientCategoryModel} from 'infrastructure/modules/patient-category/models';
+import {IPatientCategoryRepository} from 'app/modules/patient-category/repositories';
+import {IEmergencyContactRepository} from 'app/modules/emergency-contact/repositories';
+import {IPatientStatusRepository} from 'app/modules/patient-status/repositories';
+import {IUserRepository} from 'app/modules/auth/repositories';
+import {IDoctorMetadataRepository, IPatientMetadataRepository} from 'app/modules/profile/repositories';
+import {IPatientDataAccessRepository} from 'app/modules/patient-data-access/repositories';
+import {EmergencyContactModule} from 'infrastructure/modules';
+import {ContactDto} from 'domain/dtos/request/emergency-contact/contact.dto';
+
+const patient: User = {
+    id: '5nc3e70a-c1y9-121a-c5mv-5aq272098bp0',
+    email: 'patient@gmail.com',
+    firstName: 'Marc',
+    lastName: 'Goldman',
+    phone: '2930412345',
+    avatar: null,
+    role: 'Patient',
+    createdAt: '2022-10-10 07:31:17.016236',
+    deletedAt: null,
+};
+
+const emergencyContact: EmergencyContact = {
+    id: 'a9d9a7d9-0c0c-43a8-9ebf-bfbf4ecc1463',
+    userId: patient.id,
+    firstName: 'Marc',
+    lastName: 'Goldman',
+    email: 'suggested@gmail.com',
+    phone: '2930412345',
+    relationship: 'MedicalProfessional',
+    createdAt: '2022-12-10 17:31:07.016236',
+};
+
+describe('PatientController', () => {
+    let app: INestApplication;
+    beforeAll(async () => {
+        const mockedUserRepository = {
+            getOneById: jest.fn(() => Promise.resolve(patient)),
+            getByIds: jest.fn(() => Promise.resolve([patient])),
+        };
+        const mockedEmergencyContactRepository = {
+            getOneById: jest.fn(() => Promise.resolve(emergencyContact)),
+            create: jest.fn(() => Promise.resolve()),
+            delete: jest.fn(() => Promise.resolve()),
+            update: jest.fn(() => Promise.resolve()),
+            getByUserId: jest.fn(() => Promise.resolve([emergencyContact])),
+        };
+        const moduleRef: TestingModule = await Test.createTestingModule({
+            imports: [TestModule, EmergencyContactModule],
+        })
+            .overrideProvider(getRepositoryToken(UserModel))
+            .useValue(null)
+            .overrideProvider(getRepositoryToken(DoctorMetadataModel))
+            .useValue(null)
+            .overrideProvider(getRepositoryToken(PatientMetadataModel))
+            .useValue(null)
+            .overrideProvider(getRepositoryToken(EmergencyContactModel))
+            .useValue(null)
+            .overrideProvider(getRepositoryToken(PatientDataAccessModel))
+            .useValue(null)
+            .overrideProvider(getRepositoryToken(PatientStatusModel))
+            .useValue(null)
+            .overrideProvider(getRepositoryToken(PatientCategoryModel))
+            .useValue(null)
+            .overrideProvider(IPatientVitalThresholdsRepository)
+            .useValue(null)
+            .overrideProvider(IPatientCategoryRepository)
+            .useValue(null)
+            .overrideProvider(IEmergencyContactRepository)
+            .useValue(mockedEmergencyContactRepository)
+            .overrideProvider(IPatientStatusRepository)
+            .useValue(null)
+            .overrideProvider(IUserRepository)
+            .useValue(mockedUserRepository)
+            .overrideProvider(IPatientMetadataRepository)
+            .useValue(null)
+            .overrideProvider(IDoctorMetadataRepository)
+            .useValue(null)
+            .overrideProvider(IPatientDataAccessRepository)
+            .useValue(null)
+            .compile();
+
+        app = moduleRef.createNestApplication();
+        app.useGlobalPipes(new ValidationPipe());
+        await app.init();
+    });
+
+    it(`/patient/my-emergency-contact (POST)`, async () => {
+        const dto: ContactDto = {
+            firstName: 'firstName',
+            lastName: 'lastName',
+            email: 'email@gmail.com',
+            phone: '2930412345',
+            relationship: 'MedicalProfessional',
+        };
+        return request(app.getHttpServer())
+            .post('/patient/my-emergency-contact')
+            .send(dto)
+            .set('Authorization', 'Bearer patient')
+            .expect(201);
+    });
+
+    it(`/patient/my-emergency-contacts (GET)`, async () => {
+        return request(app.getHttpServer())
+            .get('/patient/my-emergency-contacts')
+            .set('Authorization', 'Bearer patient')
+            .expect(200)
+            .expect([
+                {
+                    contactId: emergencyContact.id,
+                    firstName: emergencyContact.firstName,
+                    lastName: emergencyContact.lastName,
+                    email: emergencyContact.email,
+                    phone: emergencyContact.phone,
+                    relationship: emergencyContact.relationship,
+                    createdAt: emergencyContact.createdAt,
+                },
+            ]);
+    });
+
+    it(`/patient/my-emergency-contact/:contactId (PATCH)`, async () => {
+        const dto: ContactDto = {
+            firstName: 'firstNameNew',
+            lastName: 'lastNameNew',
+            email: 'emailnew@gmail.com',
+            phone: '2930412325',
+            relationship: 'Caregiver',
+        };
+        return request(app.getHttpServer())
+            .patch(`/patient/my-emergency-contact/${emergencyContact.id}`)
+            .send(dto)
+            .set('Authorization', 'Bearer patient')
+            .expect(200);
+    });
+
+    it(`/patient/my-emergency-contact/:contactId (DELETE)`, async () => {
+        return request(app.getHttpServer())
+            .delete(`/patient/my-emergency-contact/${emergencyContact.id}`)
+            .set('Authorization', 'Bearer patient')
+            .expect(204);
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+});
