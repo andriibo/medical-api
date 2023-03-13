2e3c0c0b39417e1d31457d67d819c0de610d47ec
random <andrey.bondarenko@techs.com.ua>
2023-03-13 14:25:02 +0200

    ZZMED-559: change password

--- src/application/modules/profile/use-cases/change-password.use-case.ts
+++ src/application/modules/profile/use-cases/change-password.use-case.ts
@@ -1,10 +1,15 @@
 import {IAuthService} from 'app/modules/auth/services/auth.service';
 import {ChangePasswordDto} from 'domain/dtos/request/auth/change-password.dto';
+import {ProfileSpecification} from 'app/modules/profile/specifications/profile.specification';
 
 export class ChangePasswordUseCase {
-    public constructor(private readonly authService: IAuthService) {}
+    public constructor(
+        private readonly authService: IAuthService,
+        private readonly profileSpecification: ProfileSpecification,
+    ) {}
 
     public async changePassword(dto: ChangePasswordDto): Promise<void> {
+        this.profileSpecification.assertUserCanChangePassword(dto.currentPassword, dto.newPassword);
         await this.authService.changePassword(dto);
     }
 }
